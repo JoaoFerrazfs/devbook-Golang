167d397ca4c8312703adf090ced35c70d5319d9a
Joao Pedro Ferraz Soares <joaoferrazp@gmail.com>
2023-03-25 17:13:27 -0300

    feat: like botton feature at user publications

--- webapp/src/controllers/paginas.go
+++ webapp/src/controllers/paginas.go
@@ -1,10 +1,15 @@
 package controllers
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
+	"strconv"
 	"webapp/src/config"
+	"webapp/src/cookies"
+	"webapp/src/modelos"
 	"webapp/src/requisicoes"
+	"webapp/src/respostas"
 	"webapp/src/utils"
 )
 
@@ -18,8 +23,36 @@ func CarregarPaginaDeCadastroDeUsuario(w http.ResponseWriter, r *http.Request) {
 
 func CarregarPaginaPrincipal(w http.ResponseWriter, r *http.Request) {
 	url := fmt.Sprintf("%s/publicacoes", config.ApiUrl)
-	reponse, erro := requisicoes.FazerRequsicaoComAutentificacao(r, http.MethodGet, url, nil)
-	fmt.Println(reponse.StatusCode, erro)
+	response, erro := requisicoes.FazerRequsicaoComAutentificacao(r, http.MethodGet, url, nil)
 
-	utils.ExecutarTemplate(w, "home.html", nil)
+	if erro != nil {
+		respostas.JSON(w, http.StatusInternalServerError, respostas.ErroAPI{Erro: erro.Error()})
+		return
+	}
+
+	if response.StatusCode >= 400 {
+		respostas.TratarStatusCodeDeErro(w, response)
+		return
+	}
+
+	defer response.Body.Close()
+
+	var publicacoes []modelos.Publicacao
+
+	if erro = json.NewDecoder(response.Body).Decode(&publicacoes); erro != nil {
+		respostas.JSON(w, http.StatusUnprocessableEntity, respostas.ErroAPI{Erro: erro.Error()})
+		return
+	}
+
+	cookie, _ := cookies.Ler(r)
+
+	usuarioID, _ := strconv.ParseUint(cookie["id"], 10, 64)
+
+	utils.ExecutarTemplate(w, "home.html", struct {
+		Publicacoes []modelos.Publicacao
+		UsuarioID   uint64
+	}{
+		Publicacoes: publicacoes,
+		UsuarioID:   usuarioID,
+	})
 }
--- webapp/src/utils/templates.go
+++ webapp/src/utils/templates.go
@@ -10,6 +10,7 @@ var templates *template.Template
 // Carregar templates insere os templates html na vaiavel templates
 func CarregarTemplates() {
 	templates = template.Must(template.ParseGlob("views/*.html"))
+	templates = template.Must(templates.ParseGlob("views/templates/*.html"))
 }
 
 // Executar template rendereriza uma pagina html na tela
