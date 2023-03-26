82462d5c94bb349b2f9a4395b779d557eba4fda7
Joao Pedro Ferraz Soares <joaoferrazp@gmail.com>
2023-03-26 18:04:25 -0300

    feat: add like functions to publications

--- webapp/src/controllers/publicacoes.go
+++ webapp/src/controllers/publicacoes.go
@@ -71,3 +71,30 @@ func CurtirPublicacao(w http.ResponseWriter, r *http.Request) {
 	}
 	respostas.JSON(w, response.StatusCode, nil)
 }
+
+// Chama a API para descurtir uma publicacao
+func DescurtirPublicacao(w http.ResponseWriter, r *http.Request) {
+	parametros := mux.Vars(r)
+	fmt.Println(parametros)
+	publicacaoID, erro := strconv.ParseUint(parametros["publicacaoId"], 10, 64)
+	if erro != nil {
+		respostas.JSON(w, http.StatusBadRequest, respostas.ErroAPI{Erro: erro.Error()})
+		return
+	}
+
+	url := fmt.Sprintf("%s/publicacoes/%d/descurtir", config.ApiUrl, publicacaoID)
+
+	response, erro := requisicoes.FazerRequsicaoComAutentificacao(r, http.MethodPost, url, nil)
+	if erro != nil {
+		respostas.JSON(w, http.StatusInternalServerError, respostas.ErroAPI{Erro: erro.Error()})
+		return
+	}
+
+	defer response.Body.Close()
+
+	if response.StatusCode >= 400 {
+		respostas.TratarStatusCodeDeErro(w, response)
+		return
+	}
+	respostas.JSON(w, response.StatusCode, nil)
+}
